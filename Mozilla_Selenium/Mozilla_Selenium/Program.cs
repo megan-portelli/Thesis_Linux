bb7b07754feab06dbc7da56757c9c472b6cb9881
megan-portelli <meganportelli15@gmail.com>
2023-03-24 09:32:04 +0100

    Added Exceptions + writing to file

--- Mozilla_Selenium/Mozilla_Selenium/Program.cs
+++ Mozilla_Selenium/Mozilla_Selenium/Program.cs
@@ -1,6 +1,14 @@
 ﻿using OpenQA.Selenium.Firefox;
 
-List<string> inputs = System.IO.File.ReadAllLines("C:\\Users\\Win10 User\\source\\repos\\Thesis\\Mozilla_Selenium\\inputs.txt").ToList();
+/// <summary>
+/// This project will be to simulate a Golden Standard Parser, which
+/// in this case will be the Mozilla Firefox Parser. It will be tested
+/// by simply performing a search on the Mozilla Firefox browser and
+/// any exceptions will be recorded in a text file for further investigation.
+/// </summary>
+
+List<string> inputs = System.IO.File.ReadAllLines("C:\\Users\\Win10 User\\source\\repos\\Thesis_Linux\\Mozilla_Selenium\\Mozilla_Selenium\\Inputs\\inputs.txt").ToList();
+string resultFilePath = "C:\\Users\\Win10 User\\source\\repos\\Thesis_Linux\\Mozilla_Selenium\\Mozilla_Selenium\\Results\\results.txt";
 
 var options = new FirefoxOptions();
 
@@ -8,17 +16,25 @@ var options = new FirefoxOptions();
 var driver = new FirefoxDriver(options);
 driver.Manage().Window.Maximize();
 
-try
+foreach(string input in inputs)
 {
-    foreach (string input in inputs)
+    try
     {
         driver.Navigate().GoToUrl(input);
     }
+    catch (OpenQA.Selenium.WebDriverArgumentException webDriverArgEx)
+    {
+        File.AppendAllText(resultFilePath, "\nURL: " + input +" "+ webDriverArgEx.Message);
+    }
+    catch (OpenQA.Selenium.WebDriverException webDriverEx)
+    {
+        File.AppendAllText(resultFilePath, "\nURL: " + input + " " + webDriverEx.Message);
+    }
+    catch(Exception ex)
+    {
+        File.AppendAllText(resultFilePath, "\nURL: " + input + " " + ex.Message);
+    }
 
-}
-catch (Exception ex)
-{
-    Console.WriteLine(ex.ToString());
 }
 
 //Closing Browser
