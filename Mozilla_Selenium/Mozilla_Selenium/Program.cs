995faf4ed851b637e675b9e81dc2949d23fcfcc5
megan-portelli <meganportelli15@gmail.com>
2023-05-28 08:00:05 +0200

    Generational Results ready

--- Mozilla_Selenium/Mozilla_Selenium/Program.cs
+++ Mozilla_Selenium/Mozilla_Selenium/Program.cs
@@ -1,14 +1,15 @@
 ﻿using OpenQA.Selenium.Firefox;
 
 /// <summary>
-/// This project will be to simulate a Golden Standard Parser, which
+/// This project will be to simulate a Gold Standard Parser, which
 /// in this case will be the Mozilla Firefox Parser. It will be tested
 /// by simply performing a search on the Mozilla Firefox browser and
 /// any exceptions will be recorded in a text file for further investigation.
 /// </summary>
 
-List<string> inputs = System.IO.File.ReadAllLines("C:\\Users\\Win10 User\\source\\repos\\Thesis_Linux\\Mozilla_Selenium\\Mozilla_Selenium\\Inputs\\inputs.txt").ToList();
-string resultFilePath = "C:\\Users\\Win10 User\\source\\repos\\Thesis_Linux\\Mozilla_Selenium\\Mozilla_Selenium\\Results\\results.txt";
+List<string> inputs = System.IO.File.ReadAllLines("C:\\Users\\Win10 User\\source\\repos\\Thesis_Linux\\Mozilla_Selenium\\Mozilla_Selenium\\Inputs\\generational_urls.txt").ToList();
+string resultFilePath = "C:\\Users\\Win10 User\\source\\repos\\Thesis_Linux\\Mozilla_Selenium\\Mozilla_Selenium\\Results\\GenerationalResults.txt";
+string logsFilePath = "C:\\Users\\Win10 User\\source\\repos\\Thesis_Linux\\Mozilla_Selenium\\Mozilla_Selenium\\Logs\\GenerationalLogs.txt";
 
 var options = new FirefoxOptions();
 
@@ -20,22 +21,23 @@ foreach(string input in inputs)
 {
     try
     {
+        File.AppendAllText(logsFilePath, "Starting process for URL: " + input + "\n");
         driver.Navigate().GoToUrl(input);
+        File.AppendAllText(logsFilePath, "URL: " + input + "parsed successfully\n");
     }
     catch (OpenQA.Selenium.WebDriverArgumentException webDriverArgEx)
     {
-        File.AppendAllText(resultFilePath, "\nURL: " + input +" "+ webDriverArgEx.Message);
+        File.AppendAllText(resultFilePath, "URL: " + input +" "+ webDriverArgEx.Message + "\n");
     }
     catch (OpenQA.Selenium.WebDriverException webDriverEx)
     {
-        File.AppendAllText(resultFilePath, "\nURL: " + input + " " + webDriverEx.Message);
+        File.AppendAllText(resultFilePath, "URL: " + input + " " + webDriverEx.Message + "\n");
     }
     catch(Exception ex)
     {
-        File.AppendAllText(resultFilePath, "\nURL: " + input + " " + ex.Message);
+        File.AppendAllText(resultFilePath, "URL: " + input + " " + ex.Message + "\n");
     }
 
 }
 
-//Closing Browser
 driver.Quit();
